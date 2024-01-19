e352d835ad5e74cb25242c78adadf3fe18653ba3
tery999 <51335465+tery999@users.noreply.github.com>
2024-01-19 18:48:37 +0200

    post request mongodb
    Post request now working, MongoDB saves the information

--- server/Router/router.js
+++ server/Router/router.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Pet = require("../Models/Pets")
 
 router.post("/Pets/Add", async (req, res) =>{
-    debugger;
  const petDetails = req.body;
- console.log(petDetails);
- res.send("Send")
+ try {
+    console.log(petDetails);
+    await Pet.create(petDetails);
+ } catch (e) {
+    console.log(e);
+ }
+ res.status(200).send("Send");
 })
 
 module.exports = router;
