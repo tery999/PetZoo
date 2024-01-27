0b7a31e27558e29dd6e06f3605bc11e212fe82d2
tery999 <51335465+tery999@users.noreply.github.com>
2024-01-27 13:20:49 +0200

    overlay on image click problem
    still trying to find a solution

--- client/src/components/petDetails/PetDetailsCard.jsx
+++ client/src/components/petDetails/PetDetailsCard.jsx
@@ -1,28 +1,48 @@
+import { useState } from "react";
 import * as styles from "./PetDetails.module.css"
 
 export default function PetDetailsCard(pet) {
-    console.log(pet);
+    const [imageOverlay, setImageOverlay] = useState(false);
+
+    const clickImageHandler = () => {
+        if (imageOverlay === true) {
+            setImageOverlay(false);
+        } else {
+            setImageOverlay(true);
+        }
+    }
     return (
         <div className={styles.PetContainer}>
             <h1>NAME: {pet.name}</h1>
-            <div className={styles.Information}>
-                <p>
-                    species: {pet.species}
-                </p>
-                <p>
-                color: {pet.color}
-                </p>
-                <p>
-                age: {pet.age}
-                </p>
-                <p>
-                gender: {pet.gender}
-                </p>
-                <p>
-                description: {pet.description}
-                </p>
-                <img src={pet.image} alt="" />
+            <div className={styles.PetContainerColumn}>
+                <div className={styles.ImageDiv}>
+                  {!imageOverlay && <img src={pet.image} alt="image" className={styles.imageNotClicked} onClick={clickImageHandler}/>}  
+                  {imageOverlay && <img src={pet.image} alt="image" className={styles.imageClicked} onClick={clickImageHandler}/>}  
+                </div>
+                <div className={styles.Information}>
+                    <p>
+                        species: {pet.species}
+                    </p>
+                    <p>
+                        color: {pet.color}
+                    </p>
+                    <p>
+                        age: {pet.age}
+                    </p>
+                    <p>
+                        gender: {pet.gender}
+                    </p>
+                    <p>
+                        description: {pet.description}
+                    </p>
+                </div>
             </div>
+            <button>
+                DELETE
+            </button>
+            <button>
+                EDIT
+            </button>
         </div>
     )
 }
