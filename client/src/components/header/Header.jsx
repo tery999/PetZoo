989167ef518a96cce261260af1030a7a42caeb70
tery999 <51335465+tery999@users.noreply.github.com>
2024-02-05 20:00:27 +0200

    dropdown menu
    TODO: fix colors

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,50 +1,55 @@
 import { Link, NavLink } from "react-router-dom"
 import { UserContext } from "../../contexts/userContext"
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import * as styles from "./header.module.css"
 
 
 export default function Header() {
     const { logged } = useContext(UserContext);
+   
     return (
-        <div>
-            <div>
-                <Link to="/">
+        <div className={styles.header}>
+            <div className={styles.menu}>
+                <button>Menu</button>
+                <div className={styles.hiddenLinksDiv}>
+                <Link to={"/"}>
                     Home
                 </Link>
-            </div>
-            <div>
-                <Link to="/Pets">
+                <Link to={"/Pets"}>
                     Pets
                 </Link>
+                <Link to={"/"}>
+                    Home
+                </Link>
+                </div>
             </div>
-            {logged &&
-                <>
-                    <div>
-                        <Link to="/Pets/Add">
-                            Add Pet
-                        </Link>
-                    </div>
-                    <div>
-                        <Link to="/Logout">
-                            Logout
-                        </Link>
-                    </div>
-                </>
+
+            <div className={styles.userLogInOut}>
+                {!logged &&
+                    <>
+                        <div>
+                            <Link to="/Login">
+                                Login
+                            </Link>
+                        </div>
+                        <div>
+                            <Link to="/Register">
+                                Register
+                            </Link>
+                        </div>
+                    </>
                 }
-            {!logged &&
-                <>
-                    <div>
-                        <Link to="/Login">
-                            Login
-                        </Link>
-                    </div>
-                    <div>
-                        <Link to="/Register">
-                            Register
-                        </Link>
-                    </div>
-                </>
-            }
+
+                {logged &&
+                    <>
+                        <div>
+                            <Link to="/Logout">
+                                Logout
+                            </Link>
+                        </div>
+                    </>
+                }
+            </div>
 
         </div>
     )
